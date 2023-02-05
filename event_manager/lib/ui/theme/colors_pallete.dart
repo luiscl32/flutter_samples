c16c497ebf88add5cd8015a4d0e5d069ac258a2c
luis correa <luiscl32@gmail.com>
2023-02-04 20:03:24 -0500

    feat: :art: adding color pallete

---
+++ event_manager/lib/ui/theme/colors_pallete.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0Xff643dfb);
+  static const Color orangeDark = Color(0XffFF8A00);
+  static const Color blueDark = Color(0Xff1c1243);
+  static const Color greyMedium = Color(0XffA29EB6);
+  static const Color greyLight = Color(0XffEFF1F3);
+  static const Color white = Color(0XffFFFFFF);
+  static const Color orange = Color(0XffFF6A5D);
+  static const Color green = Color(0Xff47C272);
+
+  //* overlays
+  static const Color pink = Color(0XffE15A93);
+  static const Color blueLight = Color(0XffB37BE7);
+  static const Color greyOverlay = Color(0XffDEEDED);
+  static const Color yellowLight = Color(0XffFFE7CC);
+  static const Color pinkMedium = Color(0XffF4D8E8);
+  static const Color pinkLight = Color(0XffFFD74D);
+  static const Color blueDarkOverlay = Color.fromRGBO(28, 18, 67, 0.8);
+}
